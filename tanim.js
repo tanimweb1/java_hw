52504097a248a3ce065ebcb08142785022eca1f6
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2024-03-27 19:29:33 +0600

    Update tanim.js

--- tanim.js
+++ tanim.js
@@ -13,3 +13,32 @@ let totalMark = 5*60;
 for(let i=1; i<student; i++){
   console.log(totalMark*i);
 }
+
+const A = [50, 107, 'jack', 83, 'killer', 'son', true];
+
+const Strings =A.filter(item => typeof item === 'string');
+console.log("new:" , Strings);
+
+const Numbers =A.filter(item => typeof item === 'number');
+function compareNumbers(A, B) {
+    return A - B;
+  }
+Numbers.sort(compareNumbers);
+console.log("new-1:" , Numbers);
+
+console.log("new-2:" , Strings.reverse());
+
+const AddStrings = Strings.concat(Numbers); 
+console.log("new-3:" , AddStrings);
+
+AddStrings.push('Class','One','Two','Three','Four');
+AddStrings.splice(6, 3);
+console.log("new-4:" , AddStrings);
+
+for (var i = 0; i < Numbers.length; i++) {
+    if (Numbers[i] < 100 ) {
+        largest = Numbers[i];
+      }
+  }
+
+console.log("new-5:" , Strings.map(item => item.slice(0, -1) + '`s'));
